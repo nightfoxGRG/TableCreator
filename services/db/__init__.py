e8b32e7619b2b23e70bf90921c21dcfd1bf5d420
nightfoxGRG <nightfox@inbox.ru>
2026-04-27 19:21:22 +0300

    add functional

--- services/db/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Пакет для работы с базой данных."""
-from config.sqlalchemy.bd_engine_config import engine
-from config.sqlalchemy.db_session_config import get_session
-
-__all__ = ['engine', 'get_session']
-
